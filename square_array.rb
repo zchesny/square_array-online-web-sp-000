e9d235f041be4d028f1b6159c721a018a66c99ca
Zoey Chesny <z.chesny@gmail.com>
2020-04-09 18:20:34 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
 def square_array(array)
   # your code here
+  square_array = []
+  array.each do |number|
+    square_array << number ** 2
+  end
+  square_array
 end
+
+def square_array_map(array)
+  array.map{|number| number ** 2}
+end
+
+array = [1, 2, 3, 4, 5]
+puts square_array(array)
+puts square_array_map(array)
