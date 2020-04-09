68f0a71c45da928319148edb24e16bdd4a7a0d3e
Zoey Chesny <z.chesny@gmail.com>
2020-04-09 18:20:17 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -7,6 +7,10 @@ def square_array(array)
   square_array
 end
 
-def square_array_collect(array)
+def square_array_map(array)
   array.map{|number| number ** 2}
 end 
+
+array = [1, 2, 3, 4, 5]
+puts square_array(array)
+puts square_array_map(array)
